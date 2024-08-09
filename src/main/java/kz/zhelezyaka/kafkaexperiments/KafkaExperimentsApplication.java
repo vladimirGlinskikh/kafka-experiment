fd15b0bf633287a56abbf325a386c2c5bdbda599
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2024-08-09 17:32:29 +0300

    Start Project

---
+++ src/main/java/kz/zhelezyaka/kafkaexperiments/KafkaExperimentsApplication.java
@@ -0,0 +1,13 @@
+package kz.zhelezyaka.kafkaexperiments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaExperimentsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaExperimentsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kz/zhelezyaka/kafkaexperiments/KafkaExperimentsApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.zhelezyaka.kafkaexperiments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaExperimentsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
